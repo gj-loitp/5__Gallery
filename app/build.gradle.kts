c90c55f1d42a23ff49c7ee69630cb4dffd87e6cd
roy <roy93group@gmail.com>
2024-06-06 10:52:30 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.mckimquyen.gallery"
         minSdk = 23
         targetSdk = 34
-        versionName = "2024.06.05"
-        versionCode = 20240605
+        versionName = "2024.06.06"
+        versionCode = 20240606
         setProperty("archivesBaseName", "Cat Gallery-$versionCode")
     }
 
